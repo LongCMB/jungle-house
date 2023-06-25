53fb00b43adcabb22f2bc3d14e8cce8deed842cc
LongCMB <chenxianlongvip@gmail.com>
2023-06-25 15:50:30 +0200

    final

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,9 +1,14 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
+function handleClick(plantName) {
+	alert(`You want to buy 1 ${plantName}? Very good choice 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light, price }) {
 	return (
-		<li key={id} className='jh-plant-item'>
+		<li className='jh-plant-item' onClick={() => handleClick}>
+			<span className='jh-plant-item-price'>{price}€</span>
 			<img className='jh-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
